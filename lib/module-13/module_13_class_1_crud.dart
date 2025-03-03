c0a1af97050ec385014db43cb2918373b4c0d600
Md Rashedul Islam <santorasu09@gmail.com>
2025-03-03 22:47:54 +0600

    This is class 2 of crud app

--- lib/module-13/module_13_class_1_crud.dart
+++ lib/module-13/module_13_class_1_crud.dart
@@ -1,7 +1,6 @@
 import 'package:assignment_3/module-13/product_controller.dart';
 import 'package:flutter/material.dart';
 
-
 class Module13Class1Crud extends StatefulWidget {
   const Module13Class1Crud({super.key});
 
@@ -12,80 +11,102 @@ class Module13Class1Crud extends StatefulWidget {
 class _Module13Class1CrudState extends State<Module13Class1Crud> {
   final ProductController productController = ProductController();
 
-  void productDialog() {
+  void productDialog({
+    String? id,
+    String? name,
+    int? qty,
+    String? img,
+    int? unitPrice,
+    int? totalPrice,
+  }) {
     TextEditingController productNameController = TextEditingController();
     TextEditingController productCodeController = TextEditingController();
     TextEditingController productQuantityController = TextEditingController();
     TextEditingController productImageController = TextEditingController();
     TextEditingController productUnitPriceController = TextEditingController();
     TextEditingController productTotalPriceController = TextEditingController();
+
+    productNameController.text = name ?? '';
+    productQuantityController.text = qty != null ? qty.toString() : '0';
+    productImageController.text = img ?? '';
+    productUnitPriceController.text = unitPrice.toString() ?? '0';
+    productTotalPriceController.text = totalPrice.toString() ?? '0';
     showDialog(
-        context: context,
-        builder: (context) => AlertDialog(
-              title: Text("Add Products"),
-              content: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  TextField(
-                    controller: productNameController,
-                    decoration: InputDecoration(
-                      labelText: 'Product Name',
-                    ),
-                  ),
+      context: context,
+      builder:
+          (context) => AlertDialog(
+            title: Text(id == null ? "Add Products" : "Update Product"),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  controller: productNameController,
+                  decoration: InputDecoration(labelText: 'Product Name'),
+                ),
 
-                  TextField(
-                    controller: productImageController,
-                    decoration: InputDecoration(
-                      labelText: 'Product Image',
+                TextField(
+                  controller: productImageController,
+                  decoration: InputDecoration(labelText: 'Product Image'),
+                ),
+                TextField(
+                  controller: productQuantityController,
+                  decoration: InputDecoration(labelText: 'Product Quantity'),
+                ),
+                TextField(
+                  controller: productUnitPriceController,
+                  decoration: InputDecoration(labelText: 'Product Unit Price'),
+                ),
+                TextField(
+                  controller: productTotalPriceController,
+                  decoration: InputDecoration(labelText: 'Total Price'),
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text('Close'),
                     ),
-                  ),
-                  TextField(
-                    controller: productQuantityController,
-                    decoration: InputDecoration(
-                      labelText: 'Product Quantity',
-                    ),
-                  ),
-                  TextField(
-                    controller: productUnitPriceController,
-                    decoration: InputDecoration(
-                      labelText: 'Product Unit Price',
-                    ),
-                  ),
-                  TextField(
-                    controller: productTotalPriceController,
-                    decoration: InputDecoration(
-                      labelText: 'Total Price',
-                    ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Row(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      TextButton(
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          child: Text('Close')),
-                      SizedBox(
-                        width: 5,
+                    SizedBox(width: 5),
+                    ElevatedButton(
+                      onPressed: () {
+                        if (id == null) {
+                          productController.createProduct(
+                            productNameController.text,
+                            productImageController.text,
+                            int.parse(productQuantityController.text),
+                            int.parse(productUnitPriceController.text),
+                            int.parse(productTotalPriceController.text),
+                          );
+                        } else {
+                          productController.updateProduct(
+                            id,
+                            productNameController.text,
+                            productImageController.text,
+                            int.parse(productQuantityController.text),
+                            int.parse(productUnitPriceController.text),
+                            int.parse(productTotalPriceController.text),
+                          );
+                        }
+
+                        fetchData();
+                        Navigator.pop(context);
+                        setState(() {});
+                      },
+                      child: Text(
+                        id == null ? "Add Products" : "Update Product",
                       ),
-                      ElevatedButton(
-                          onPressed: () {
-                            setState(() {
-                              productController.createProducts(productNameController.text, productImageController.text, int.parse(productQuantityController.text), int.parse(productUnitPriceController.text), int.parse(productTotalPriceController.text));
-                              fetchData();
-                              Navigator.pop(context);
-                            });
-
-                          }, child: Text('Add Product')),
-                    ],
-                  )
-                ],
-              ),
-            ));
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+    );
   }
 
   Future<void> fetchData() async {
@@ -105,47 +126,76 @@ class _Module13Class1CrudState extends State<Module13Class1Crud> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          title: const Text("Products"),
-        ),
+        appBar: AppBar(title: const Text("Products")),
         body: ListView.builder(
-            itemCount: productController.products.length,
-            itemBuilder: (context, index) {
-              var products = productController.products[index];
-              return Card(
-                elevation: 4,
-                margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                child: ListTile(
-                  // leading: Image.network(
-                  //   products['Img'],
-                  //   width: 150,
-                  //   fit: BoxFit.contain,
-                  // ),
-                  title: Text(
-                    products.productName.toString(),
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  subtitle: Text("Price: \$ ${products.unitPrice} | Quantity: ${products.qty}\nTotal Price: ${products.totalPrice}"),
-                  trailing: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      IconButton(
-                          onPressed: () => productDialog(),
-                          icon: Icon(Icons.edit)),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      IconButton(
-                          onPressed: () {},
-                          icon: Icon(
-                            Icons.delete,
-                            color: Colors.red,
-                          ))
-                    ],
-                  ),
+          itemCount: productController.products.length,
+          itemBuilder: (context, index) {
+            var product = productController.products[index];
+            return Card(
+              elevation: 4,
+              margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              child: ListTile(
+                // leading: Image.network(
+                //   products['Img'],
+                //   width: 150,
+                //   fit: BoxFit.contain,
+                // ),
+                title: Text(
+                  product.productName.toString(),
+                  style: TextStyle(fontWeight: FontWeight.bold),
                 ),
-              );
-            }),
+                subtitle: Text(
+                  "Price: \$ ${product.unitPrice} | Quantity: ${product.qty}\nTotal Price: ${product.totalPrice}",
+                ),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(
+                      onPressed:
+                          () => productDialog(
+                            id: product.sId,
+                            name: product.productName,
+                            img: product.img,
+                            unitPrice: product.unitPrice,
+                            totalPrice: product.totalPrice,
+                          ),
+                      icon: Icon(Icons.edit),
+                    ),
+                    SizedBox(width: 10),
+                    IconButton(
+                      onPressed: () {
+
+                          productController
+                              .deleteProducts(product.sId.toString())
+                              .then((value) {
+                                if (value) {
+                                  setState(() {
+                                    fetchData();
+                                  });
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(
+                                      content: Text("Product Deleted"),
+                                      duration: Duration(seconds: 2),
+                                    ),
+                                  );
+                                } else {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(
+                                      content: Text("Please try again.."),
+                                      duration: Duration(seconds: 2),
+                                    ),
+                                  );
+                                }
+                              });
+                      },
+                      icon: Icon(Icons.delete, color: Colors.red),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
         floatingActionButton: FloatingActionButton(
           onPressed: () => productDialog(),
           child: Icon(Icons.add),
